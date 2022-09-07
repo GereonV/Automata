bd25c1d77e063bf441f0a195be536dc0fc0fe3dd
Gereon V <72784429+GereonV@users.noreply.github.com>
2022-09-07 22:01:10 +0200

    Add evaluation

--- src/automata.hpp
+++ src/automata.hpp
@@ -1,6 +1,7 @@
 #ifndef _AUTOMATA_HPP_
 #define _AUTOMATA_HPP_
 
+#include <algorithm>
 #include <map>
 #include <ranges>
 #include <set>
@@ -9,16 +10,44 @@
 
 namespace fa {
 
+    inline constexpr auto map = [](std::map<char, std::size_t> const & alphabet) { return [&](char c) { return alphabet.at(c); }; };
+
+    template<typename T>
+    struct it_pair {
+        T pair;
+        constexpr auto begin() const noexcept { auto && [b, _] = pair; return b; }
+        constexpr auto end()   const noexcept { auto && [_, e] = pair; return e; }
+    };
+
     struct nfa {
         std::vector<std::pair<std::multimap<std::size_t, std::size_t>, bool>> states;
         std::size_t start{};
     };
+
+    bool evaluate(auto && input, nfa const & nfa) noexcept {
+        std::set curr{nfa.start};
+        for(auto c : input) {
+            std::set<std::size_t> next;
+            for(auto && state : curr)
+                for(auto && s : it_pair{nfa.states[state].first.equal_range(c)})
+                    next.emplace(s.second);
+            curr.swap(next);
+        }
+        return std::ranges::any_of(curr, [&](auto state) { return nfa.states[state].second; });
+    }
 
     struct dfa {
         std::vector<std::size_t> delta;
         std::set<std::size_t> accepting_states;
         std::size_t start;
     };
+
+    bool evaluate(auto && input, dfa const & dfa, std::size_t alphabet_size) noexcept {
+        auto curr = dfa.start;
+        for(auto c : input)
+            curr = dfa.delta[curr * alphabet_size + c];
+        return dfa.accepting_states.contains(curr);
+    }
 
     dfa nfa_to_dfa(nfa const & nfa, std::size_t alphabet_size) noexcept {
         dfa dfa;
@@ -32,8 +61,7 @@ namespace fa {
                 bool accepting{};
                 std::set<std::size_t> og_successors;
                 for(auto og_state : state) {
-                    auto && [begin, end] = nfa.states[og_state].first.equal_range(i);
-                    for(auto [_, succ] : std::ranges::subrange{begin, end}) {
+                    for(auto [_, succ] : it_pair{nfa.states[og_state].first.equal_range(i)}) {
                         og_successors.emplace(succ);
                         if(nfa.states[succ].second)
                             accepting = true;
--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,6 @@
 #include "automata.hpp"
 
 int main() {
-    std::size_t alphs{2};
     fa::nfa nfa{{
         {{
             {0, 0},
@@ -14,7 +13,12 @@ int main() {
             {1, 1}
         }, true}
     }};
+    auto alphs = nfa.states.size();
+    std::cout << evaluate(std::initializer_list{0, 1, 1, 0}, nfa) << '\n';
+    std::cout << evaluate(std::initializer_list{1, 0, 1, 1}, nfa) << '\n';
     auto dfa = fa::nfa_to_dfa(nfa, alphs);
+    std::cout << evaluate(std::initializer_list{0, 1, 1, 0}, dfa, alphs) << '\n';
+    std::cout << evaluate(std::initializer_list{1, 0, 1, 1}, dfa, alphs) << '\n';
     for(int i{}; i < dfa.delta.size() / alphs; ++i) {
         std::cout << "State " << i << (dfa.accepting_states.contains(i) ? " (accepting)" : "") << ":\n";
         for(int j{}; j < alphs; ++j)
