ef3cd117d1a88c39c9c2ed5b1399279f6749e16a
Gereon V <72784429+GereonV@users.noreply.github.com>
2022-09-08 21:41:12 +0200

    Rework tests

--- src/main.cpp
+++ src/main.cpp
@@ -17,20 +17,25 @@ int main() {
         }, true}
     }};
     auto alphs = alphabet.size(); // nfa.states.size()
-    std::cout << evaluate(std::initializer_list{0, 1, 1, 0}, nfa) << '\n';
-    std::cout << evaluate(std::initializer_list{1, 0, 1, 1}, nfa) << '\n';
     auto dfa = fa::nfa_to_dfa(nfa, alphs);
-    std::cout << evaluate(std::initializer_list{0, 1, 1, 0}, dfa, alphs) << '\n';
-    std::cout << evaluate(std::initializer_list{1, 0, 1, 1}, dfa, alphs) << '\n';
-    std::cout << evaluate("ABBA"sv | fa::map(alphabet), nfa) << '\n';
-    std::cout << evaluate("BABA"sv | fa::map(alphabet), nfa) << '\n';
-    std::cout << evaluate("ABAB"sv | fa::map(alphabet), nfa) << '\n';
-    std::cout << evaluate("ABBA"sv | fa::map(alphabet), dfa, alphs) << '\n';
-    std::cout << evaluate("BABA"sv | fa::map(alphabet), dfa, alphs) << '\n';
-    std::cout << evaluate("ABAB"sv | fa::map(alphabet), dfa, alphs) << '\n';
-    for(int i{}; i < dfa.delta.size() / alphs; ++i) {
+    auto not_equal = [&](auto && in, bool should) { auto ans = evaluate(in, dfa, alphs); return ans ^ should || ans ^ evaluate(in, nfa); };
+    if(not_equal(std::initializer_list{0, 1, 1, 0}, false) ||
+       not_equal(std::initializer_list{1, 0, 1, 0}, false) ||
+       not_equal(std::initializer_list{1, 0, 1, 1}, true)  ||
+       not_equal("ABBA"sv | fa::map(alphabet), false)      ||
+       not_equal("BABA"sv | fa::map(alphabet), false)      ||
+       not_equal("ABAB"sv | fa::map(alphabet), true))
+        return -1;
+    for(std::size_t i{}; i < dfa.delta.size() / alphs; ++i) {
         std::cout << "State " << i << (dfa.accepting_states.contains(i) ? " (accepting)" : "") << ":\n";
-        for(int j{}; j < alphs; ++j)
+        for(std::size_t j{}; j < alphs; ++j)
+            std::cout << j << ':' << dfa.delta[i * alphs + j] << '\n';
+    }
+    std::cout << std::endl;
+    dfa = minimize(dfa, alphs);
+    for(std::size_t i{}; i < dfa.delta.size() / alphs; ++i) {
+        std::cout << "State " << i << (dfa.accepting_states.contains(i) ? " (accepting)" : "") << ":\n";
+        for(std::size_t j{}; j < alphs; ++j)
             std::cout << j << ':' << dfa.delta[i * alphs + j] << '\n';
     }
 }
